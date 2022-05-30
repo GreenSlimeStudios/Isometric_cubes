55f4b3278a0b3a29c53d8553800f9dfed05378cd
Max Berliński <maxber.gdynia@gmail.com>
2022-05-30 13:25:26 +0200

    nice additions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 
 use macroquad::prelude::*;
 
+const STRANGE_THINGS:bool = true;
+
 #[macroquad::main("Texture")]
 async fn main() {
+    //Here declare the x axis and the y axis of the isometric plane
     let xax = 16;
     let yax =16;
 
@@ -21,8 +24,8 @@ async fn main() {
 
         for i in 0..xax{
             for j in (0..yax).rev(){
-                let x = (cubes[i*yax+j].x as f32 * 0.5 * 32.0 + cubes[i*yax+j].y as f32 * 0.5 * 32.0) + 0.2 * screen_width();
-                let y = (cubes[i*yax+j].x as f32 * 0.25 * 32.0 + cubes[i*yax+j].y as f32 * -0.25 * 32.0) + 0.5 * screen_height();
+                let x = (cubes[(i*yax+j) as usize].x as f32 * 0.5 * 32.0 + cubes[(i*yax+j) as usize].y as f32 * 0.5 * 32.0) + 0.2 * screen_width();
+                let y = (cubes[(i*yax+j) as usize].x as f32 * 0.25 * 32.0 + cubes[(i*yax+j) as usize].y as f32 * -0.25 * 32.0) + 0.5 * screen_height();
                 // let y = j as f32 * 32.0;
                 draw_texture(
                     texture,
@@ -30,6 +33,34 @@ async fn main() {
                     y,
                     WHITE,
                 );
+                //===================================================
+                if STRANGE_THINGS {
+                    if i % 5 == 0 || j % 3 == 0{   
+                        up_down(&mut cubes[i*yax+j]);
+                    }
+                    else if frame > 15 {
+                        up_down(&mut cubes[i*yax+j]);
+                    }
+                }
+                fn up_down(cube:&mut Cube){
+                    if cube.cnt <= 0 {
+                        cube.is_up = true;
+                    }
+                    if cube.cnt > 15 {
+                        cube.is_up = false;
+                    }
+                    if cube.is_up{
+                        cube.updatez();
+                        cube.cnt += 1
+                    }
+                    else {
+                        cube.updatezm();
+                        cube.cnt -= 1;
+                    }
+                }
+                //=================================================
+
+
                 if frame >= 480 && frame < 520{
 
                     if i % 5 == 0 || j % 5 ==0{
@@ -164,11 +195,13 @@ async fn main() {
 struct Cube{
     x:f32,
     y:f32,
-    
+    cnt:i32,
+    is_up:bool,
+    // id:i32,
 }
 impl Cube {
     fn new(x:f32,y:f32) -> Self {
-        Self{x:x,y:y}
+        Self{x:x,y:y,cnt:0,is_up:true}
     }
     fn updatey(&mut self){
         // self.x += 0.1;
@@ -189,9 +222,12 @@ impl Cube {
     fn updatez(&mut self){
         self.x -= 0.1;
         self.y += 0.1;
+        // self.cnt += 1;
     }
     fn updatezm(&mut self){
         self.x += 0.1;
         self.y -= 0.1;
+        // self.cnt -= 1;
+
     }
 }
